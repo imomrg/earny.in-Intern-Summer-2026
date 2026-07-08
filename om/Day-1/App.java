9a3bece3d18bd6cbffe6e0a5aa05ac405d66d2c8
connectmeom-droid <connectme.rajom@gmail.com>
2026-07-08 22:21:47 +0530

    updated

--- om/Day-1/App.java
+++ om/Day-1/App.java
@@ -1,5 +1,33 @@
+// public class App {
+//     public static void main(String[] args) throws Exception {
+//         System.out.println("Hi earny.in - i am om, nice to meet you");
+//     }
+// }
+import java.util.Scanner;
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hi earny.in - i am om, nice to meet you");
-    }
+   public static void main(String[] args) {
+     
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter first sub marks: ");
+    int sub1 = sc.nextInt();
+
+    System.out.println("Enter second sub marks: ");
+    int sub2 = sc.nextInt();
+
+    System.out.println("Enter third sub marks: ");
+    int sub3 = sc.nextInt();
+
+    System.out.println("Enter fourth sub marks: ");
+    int sub4 = sc.nextInt();
+
+    System.out.println("Enter fifth sub marks: ");
+    int sub5 = sc.nextInt();
+
+
+    int totalmarks = sub1 + sub2 + sub3 + sub4 + sub5;
+    System.out.println("total marks: " + totalmarks);
+
+    float percentage = (totalmarks / 500.0f) * 100;
+    System.out.println("percentage:" + percentage);
+}   
 }
